9fb41958eb2de1e428ccce6a1656a523c0c071c0
TanThanhLe <tanthanhcr95@gmail.com>
2024-04-24 23:40:24 +0700

    Simple Python while statement example

--- main.py
+++ main.py
@@ -5,3 +5,14 @@ if __name__ == "__main__":
   while condition:
     body
   """
+
+  # Python while statement examples
+
+  # 1) Simple Python while statement example
+
+  max = 5
+  counter = 0
+
+  while counter < max:
+    print(counter)
+    counter += 1
